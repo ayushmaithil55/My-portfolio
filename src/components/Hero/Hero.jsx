729b3a21b670a9db51a535f75e198afd76938164
ayushmaithil55 <ayushmaithil55@gmail.com>
2024-11-23 23:49:18 +0530

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,14 +7,19 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Ayush Maithil</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
+          I'm a frontend developer with 1 year of experience using React and
           NodeJS. Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
+        <div className={styles.buttonContainer}>
+    <a href="mailto:ayushmaithil155@gmail.com" className={styles.contactBtn}>
+        Contact Me
+    </a>
+    <a href="https://drive.google.com/file/d/140oLCU62kyzF3a3uaKXcG1UNenhHXAan/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className={styles.resumeBtn}>
+        Resume
+    </a>
+</div>
       </div>
       <img
         src={getImageUrl("hero/heroImage.png")}
